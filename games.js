8066eb441aa8d2e55bafd6da7ce8fc5962e08ac1
lokirito <lokirito@mail.ru>
2025-10-23 22:09:52 +0700

    add hw5

--- games.js
+++ games.js
@@ -17,3 +17,38 @@ function guessNumber() {
         }
     }   
 }
+
+
+function generateMathProblem() {
+  const num1 = Math.floor(Math.random() * 10) + 1;
+  const num2 = Math.floor(Math.random() * 10) + 1;
+  const operations = ['+', '-', '*', '/'];
+  const operation = operations[Math.floor(Math.random() * operations.length)];
+  const question = `${num1} ${operation} ${num2}`;
+
+  let answer;
+  switch (operation) {
+    case '+':
+      answer = num1 + num2;
+      break;
+    case '-':
+      answer = num1 - num2;
+      break;
+    case '*':
+      answer = num1 * num2;
+      break;
+    case '/':
+      answer = (num1 / num2).toFixed(2);
+      break;
+  }
+
+  const userAnswer = prompt(`Решите: ${question}`);
+
+  if (userAnswer !== null) {
+    if (userAnswer.trim() === String(answer)) {
+      alert("Правильно!");
+    } else {
+      alert(`Ошибка`);
+    }
+  }
+}
--- script.js
+++ script.js
@@ -1,108 +1,150 @@
 // Задание 1
 
-let maxNumber = (a, b) => {
-    if (a > b) {
-        return a;
-    } else if (b > a) {
-        return b;
-    } else {
-        return console.log(`Значения равны`);
+const massiv = [1, 5, 4, 10, 0, 3];
+for (let i = 0; i < massiv.length; i++) {
+    console.log(massiv[i]);
+    if (massiv [i] === 10) break;
     }
-}
-
-console.log(maxNumber(8, 4));
-console.log(maxNumber(36, 54));
-maxNumber(6, 6);
 
 // Задание 2
 
-let parity = number => {
-    if (number % 2 === 0) {
-        console.log(`Число четное`);
-    } else {
-        console.log(`Число нечетное`);
+const mass = [1, 5, 4, 10, 0, 3];
+mass.forEach ((el,index) => {
+    if (el === 4) {
+        console.log(`Индекс 4 равен ${index}`);
     }
-}
-
-parity(6);
-parity(13);
+});
 
 // Задание 3
 
-function outputSquare(digit) {
-    let result = digit ** 2;
-    console.log(`Квадрат числа ${digit} равен ${result}`);
-}
-
-outputSquare(7);
-
-let returnSquare = numeral => numeral ** 2;
-
-console.log(returnSquare(4));
+const m = [1, 3, 5, 10, 20];
+let joinedM = m.join('');
+console.log(joinedM);
 
 // Задание 4
 
-function helloMessage(userAnswer) {
-    let hello = prompt(userAnswer);
-    if (hello >= 0 && hello <= 12) {
-        console.log(`Привет, друг!`);
-    } else if (hello > 13) {
-        console.log(`Добро пожаловать!`);
-    } else {
-        console.log(`Вы ввели неправильное значение`);
+const arr = [];
+for (let i = 0; i < 3; i++) {
+    const arr2 = [];
+    for (let i = 0; i < 3; i++) {
+        arr2.push(1);
     }
+    arr.push(arr2);
 }
-
-helloMessage (`Сколько вам лет?`);
+console.log(arr);
 
 // Задание 5
 
-let isN = (first, second) => 
-    (isNaN(first) || isNaN(second)) 
-    ? (console.log(`Одно или оба значения не являются числом`), undefined)
-    : first * second;
-
-console.log(isN (5, 7));
-console.log(isN (`ва`, 7));
-console.log(isN (`dg`, `ымы`));
+const mas = [1, 1, 1];
+mas.push(2, 2, 2);
+console.log(mas);
 
 // Задание 6
 
-function parityNumber(question) {
-    let n = Number(prompt(question));
-    if (n = isNaN) {
-        console.log(`Переданный параметр не является числом`);
-    } else {
-        final = n ** 2;
-        console.log(`${n} в кубе равняется ${final}`);
-    }
-}
-
-parityNumber (`Введите любое число`);
+const array = [9, 8, 7, 'a', 6, 5];
+array.sort();
+console.log(array);
+const filterArray = array.filter(el => el !== 'a');
+console.log(filterArray);
 
 // Задание 7
 
-function getRectangleArea() {
-    return this.radius ** 2 * 3.14;
+const numbers = [9, 8, 7, 6, 5];
+
+const userNumber = +prompt(`Угадай число из массива`);
+if (numbers.includes(userNumber)) {
+    alert(`Угадал`);
+} else {
+    alert(`Не угадал`);
 }
 
-function getRectanglePerimeter() {
-    return this.radius * 2 * 3.14;
+// Задание 8
+
+let text = 'abcdef';
+text = text.split('');
+console.log(text);
+text.reverse();
+text = text.join('');
+console.log(text);
+
+
+// Задание 9
+
+const massivTwo = [[1, 2, 3],[4, 5, 6]];
+const massivNew = []
+
+for (let i = 0; i < massivTwo.length; i++) {
+    massivNew.push(...massivTwo[i]);
+}
+console.log(massivNew);
+
+// Задание 10
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < number.length; i++) {
+    if (number[i + 1]) {
+        console.log(number[i] + number[i + 1]);
+    }
 }
 
-const circle1 = {
-    radius: 4,
-    getArea: getRectangleArea,
-    getPerimeter: getRectanglePerimeter,
+// Задание 11
+
+function square(arr) {
+    return arr.map(item => item ** 2);
 }
 
-const circle2 = {
-    radius: 7,
-    getArea: getRectangleArea,
-    getPerimeter: getRectanglePerimeter,
+console.log(square([1, 2, 3]));
+
+// Задание 12
+
+function getLength(arr) {
+    return arr.map(item => item.length);
 }
 
-console.log(circle1.getArea());
-console.log(circle1.getPerimeter());
-console.log(circle2.getArea());
-console.log(circle2.getPerimeter());
+console.log(getLength([`hello`, `word`]));
+
+// Задание 13
+
+function negativeNumbers(array) {
+    return array.filter(item => item < 0);
+}
+
+console.log(negativeNumbers([1 ,3, 7, -6, 9, -14, 2, -7]));
+
+// Задание 14
+
+function randomNumb() {
+    return Math.floor(Math.random() * 10)
+}
+const arraw = [];
+
+for (let i = 0; i < 10; i++) {
+    arraw.push(randomNumb())
+}
+
+console.log(arraw);
+
+const evenArraw = [];
+
+for (let i = 0; i < arraw.length; i++) {
+    if (arraw[i] % 2 === 0) {
+        evenArraw.push(arraw[i])
+    }
+}
+
+console.log(evenArraw);
+
+// Задание 15
+
+function randomNumber() {
+    return Math.floor(Math.random() * 10)
+}
+const arrr = [];
+
+for (let i = 0; i < 4; i++) {
+    arrr.push(randomNumber());
+}
+
+console.log(arrr);
+console.log(arrr.reduce((a, b) => a + b) / arrr.length);
